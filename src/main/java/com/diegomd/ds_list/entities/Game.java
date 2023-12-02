b55e578b5bdac48879aca5edb48a25aab44f61b5
diegomcoder <mdsidiego@gmail.com>
2023-12-01 22:23:54 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/diegomd/ds_list/entities/Game.java
@@ -0,0 +1,29 @@
+package com.diegomd.ds_list.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_game")
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
